d6930518f8d6fd7439584ea6aebce0446d77b70b
Your Name <you@example.com>
2025-01-25 12:47:00 +0530

    msg

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,6 @@ module "ec2" {
   #these are variables expected module not arguments of resources definition
   #you can also create variable or else you can directly use values here
   ami_id = var.ami_aws
-  instance_type = "t3.small"
-  security_group_ids = ["sg-0e914dc599cdf119a"]
+  instance_type = var.instance_type
+  security_group_ids = var.security_group_ids
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
 variable "ami_aws" {
     default = "ami-09c813fb71547fc4f"
 }
+variable "instance_type" {
+    default = "t3.micro"
+}
+variable "security_group_ids" {
+    type= list(string)
+}
